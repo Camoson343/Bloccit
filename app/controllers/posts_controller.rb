3b6f610723366273d67f61b540b9d3751af7facb
Cameron Chafkar <ccdecisions@gmail.com>
2019-02-16 12:08:06 -0800

    add SPAM

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,11 @@
 class PostsController < ApplicationController
   def index
-
     @posts = Post.all
+    @posts.each_with_index do |post, index|
+      if index % 5 == 0
+        post.title = "SPAM"
+      end
+    end
   end
 
   def show
@@ -12,4 +16,4 @@ class PostsController < ApplicationController
 
   def edit
   end
-end
+end 
