f8cfb9de9cd9d5a64c846e3c68c1bee29c713a6d
Cameron Chafkar <ccdecisions@gmail.com>
2019-02-24 22:35:55 -0800

    started validations

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,6 +4,14 @@ RSpec.describe Post, type: :model do
 
   let(:post) { Post.create!(title: "New Post Title", body: "New Post Body") }
 
+  it { is_expected.to validate_presence_of(:title) }
+   it { is_expected.to validate_presence_of(:body) }
+   it { is_expected.to validate_presence_of(:topic) }
+
+   it { is_expected.to validate_length_of(:title).is_at_least(5) }
+   it { is_expected.to validate_length_of(:body).is_at_least(20) }
+ 
+
   describe "attributes" do
     it "has title and body attributes" do
       expect(post).to have_attributes(title: "New Post Title", body: "New Post Body")
