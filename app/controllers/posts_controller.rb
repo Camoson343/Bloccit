728d457bce544b280e213b6513848452659c8870
Cameron Chafkar <ccdecisions@gmail.com>
2019-03-03 16:51:29 -0800

    topis and post work

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,31 +1,28 @@
 class PostsController < ApplicationController
-  def index
-
-    @posts = Post.all
-  end
 
   def show
     @post = Post.find(params[:id])
   end
 
   def new
-    #7
+    @topic = Topic.find(params[:topic_id])
     @post = Post.new
   end
 
   def create
-# #9
   @post = Post.new
   @post.title = params[:post][:title]
   @post.body = params[:post][:body]
+  @topic = Topic.find(params[:topic_id])
 
-# #10
+  @post.topic = @topic
+
   if @post.save
-# #11
+
     flash[:notice] = "Post was saved."
-    redirect_to @post
+    redirect_to [@topic, @post]
   else
-# #12
+
     flash.now[:alert] = "There was an error saving the post. Please try again."
     render :new
   end
@@ -42,7 +39,7 @@ end
 
      if @post.save
        flash[:notice] = "Post was updated."
-       redirect_to @post
+       redirect_to [@post.topic, @post]
      else
        flash.now[:alert] = "There was an error saving the post. Please try again."
        render :edit
@@ -52,10 +49,10 @@ end
    def destroy
      @post = Post.find(params[:id])
 
- # #8
+
      if @post.destroy
        flash[:notice] = "\"#{@post.title}\" was deleted successfully."
-       redirect_to posts_path
+       redirect_to @post.topic
      else
        flash.now[:alert] = "There was an error deleting the post."
        render :show
