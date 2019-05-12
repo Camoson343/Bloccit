283602e12e751bd6097d2546fe7bdc83cc0356a6
Cameron Chafkar <ccdecisions@gmail.com>
2019-05-12 09:52:24 -0700

    authorization-work

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_19_153316) do
+ActiveRecord::Schema.define(version: 2019_05_05_042613) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string "title"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2019_04_19_153316) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role"
   end
 
 end
